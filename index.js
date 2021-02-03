c2475ee30282ff8232eece1e1c50d0307b96a88d
Oliver <oliver.plummer@outlook.com>
2021-02-03 12:19:29 +1100

    Update

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+	extends: "airbnb-typescript",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: {
+		react: {
+			pragma: "createElement",
+		},
+	},
+	rules: {
+		"react/prop-types": "off",
+		"jsx-a11y/no-autofocus": "off",
+		"react/no-children-prop": "off",
+		"jsx-a11y/media-has-caption": "off",
+		"react/no-unused-prop-types": "off",
+		"react-hooks/exhaustive-deps": "off",
+		"react/require-default-props": "off",
+		"react/jsx-indent": ["error", "tab"],
+		"react/jsx-indent-props": ["error", "tab"],
+		"react/jsx-fragments": ["error", "element"],
+		"jsx-a11y/click-events-have-key-events": "off",
+		"jsx-a11y/label-has-associated-control": "off",
+		"jsx-a11y/control-has-associated-label": "off",
+		"jsx-a11y/no-static-element-interactions": "off",
+		"react/jsx-no-duplicate-props": ["error", { ignoreCase: false }],
+		"react/jsx-tag-spacing": ["error", { beforeSelfClosing: "never" }],
+	}
+}
