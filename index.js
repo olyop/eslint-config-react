7d5d75000c91d13e8184791df910838e9375b897
olyop <oliver.plummer@outlook.com>
2021-11-15 12:38:25 +1100

    Update

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+	extends: [
+		"@oly_op",
+		"airbnb",
+		"airbnb/hooks",
+		"airbnb-typescript"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: {
+		react: {
+			pragma: "createElement",
+		},
+	},
+	rules: {
+		"react/prop-types": "off",
+		"jsx-a11y/no-autofocus": "off",
+		"react/no-children-prop": "off",
+		"react/no-unused-prop-types": "off",
+		"jsx-a11y/media-has-caption": "off",
+		"react-hooks/exhaustive-deps": "off",
+		"react/require-default-props": "off",
+		"react/jsx-props-no-spreading": "off",
+		"jsx-a11y/click-events-have-key-events": "off",
+		"jsx-a11y/label-has-associated-control": "off",
+		"jsx-a11y/control-has-associated-label": "off",
+		"jsx-a11y/no-static-element-interactions": "off",
+		"react/jsx-indent": [
+			"error",
+			"tab",
+		],
+		"react/jsx-indent-props": [
+			"error",
+			"tab",
+		],
+		"react/jsx-fragments": [
+			"error",
+			"element",
+		],
+		"react/jsx-tag-spacing": [
+			"error",
+			{
+				"beforeSelfClosing": "never"
+			}
+		]
+	}
+}
